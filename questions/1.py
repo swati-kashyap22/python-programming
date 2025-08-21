a46c43bca891fef3801215000ad1fb89192635b7
swati-kashyap22 <kashyapswati0909@gmail.com>
2025-08-21 10:35:45 +0530

    add folder

---
+++ questions/1.py
@@ -0,0 +1,18 @@
+
+# n = int(input("enter n :"))
+# for i in range(0,n):
+#     print("*" , end = " ")
+
+ #  ---------------------
+
+n = int(input("enter n :"))
+m = int(input(" enter m : "))
+for i in range(1,n):
+    for j in range(1,m):
+        
+        print("*")
+        continue
+
+
+
+
